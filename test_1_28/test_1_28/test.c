aa882ca8b5d6b00b0fe12026335ddb8dd83b07b3
zhu-yan-c <yanzhaochu@qq.com>
2026-01-29 17:18:16 +0800

    嵌套调用示例代码

---
+++ test_1_28/test_1_28/test.c
@@ -0,0 +1,28 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//#include<stdbool.h>
+//_Bool is_leap_year(int y)
+//{
+//	if (((y % 4 == 0) && (y % 100 != 0)) || (y % 400 == 0))
+//		return true;
+//	else
+//		return false;
+//}
+//int get_days_of_month(int y, int m)
+//{
+//	int days[] = {0,31,28,31,30,31,30,31,31,30,31,30,31 };
+//	int day = days[m];
+//	if (is_leap_year(y) && m == 2)
+//		day += 1;
+//	return day;
+//}
+//int main()
+//{
+//	int year = 0;
+//	int month = 0;
+//	scanf("%d %d", &year, &month);
+//	int day = get_days_of_month(year, month);
+//	printf("%d\n",day);
+//	return 0;
+//}
+
